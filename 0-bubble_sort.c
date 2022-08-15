2b4d4ce13d83de2cee7241553301afd02feab19a
mdallen5393 <allen_matthew@outlook.com>
2022-08-15 10:02:34 -0700

    Added .c files for bubble and insertion sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort int arr in ascending order using bubble sort
+ * @array: array to sort
+ * @size: number of elements in array
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort DLL in ascending order using insertion
+ * @list: list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+
+}
