49990ad0a28bd4abffee81313b33ea48ed2d7bff
bsbanotto <4905@holbertonschool.com>
2022-08-15 11:44:18 -0700

    Committing bubble sort and updated header

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,21 @@
 #include "sort.h"
+/**
+ * array_swap - swaps two integers in an array
+ * @array: array to be sorted
+ * @a: index of first integer
+ * @b: index of second integer
+ *
+ * Return: Void
+ */
+
+void array_swap(int *array, int a, int b)
+{
+    int temp;
+
+    temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
+}
 
 /**
  * bubble_sort - sort int arr in ascending order using bubble sort
@@ -7,5 +24,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+    unsigned int i, newnum, temp;
 
+    do
+    {
+        newnum = 0;
+        for (i = 1; i < size; i++)
+        {
+            if (array[i - 1] > array[i])
+            {
+                array_swap(array, i - 1, i);
+                newnum = i;
+                print_array(array, size);
+            }
+        }
+        temp = newnum;
+    } while (temp >= 1);
 }
--- sort.h
+++ sort.h
@@ -25,6 +25,8 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+void array_swap(int *array, int a, int b);
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
