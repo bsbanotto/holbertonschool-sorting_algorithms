6931ac316e3931863da631befe8f9b6363914465
bsbanotto <ben.sbanotto@gmail.com>
2023-01-10 10:29:31 -0600

    Committing file for merge_sort

---
+++ 103-merge_sort.c
@@ -0,0 +1,14 @@
+#include sort.h
+
+/**
+ * merge_sort - Implementation of top-down merge sort algorithm
+ * @array: pointer to array of integers to be sorted
+ * @size: size of given array
+ *
+ * Return: void
+ */
+
+void merge_sort(int *array, size_t size)
+{
+    
+}
