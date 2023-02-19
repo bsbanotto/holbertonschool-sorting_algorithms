18082a7b6a0fb2d5d5faca2717a141ec266ea7f6
bsbanotto <ben.sbanotto@gmail.com>
2023-02-19 10:21:35 -0600

    It works when I run it

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -17,13 +17,8 @@ void radix_sort(int *array, size_t size)
 		max = getMax(array, size);
 		for (place = 1; max / place > 0; place *= 10)
 		{
-			if (place > 1)
-			{
-				print_array(array, size);
-			}
 			countingSort(array, size, place);
-			if (max / place < 10)
-				print_array(array, size);
+			print_array(array, size);
 		}
 	}
 }
